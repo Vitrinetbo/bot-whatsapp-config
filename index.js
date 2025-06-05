e9700d631aee09270eba1f111ba1a15e0fa70eaf
Vitrinetbo <vitrine.tbo@gmail.com>
2025-06-04 22:06:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-// Inicialize Firebase Admin SDK
-const serviceAccount = require('./serviceAccountKey.json'); // baixa no Firebase Console > Configurações > Contas de serviço
+// Inicialize Firebase Admin SDK usando variáveis de ambiente
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // importante para as quebras de linha
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
